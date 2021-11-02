928c28c6ea9a8f65e1f67616c5b211bad3f9e71c
bwaah1 <chykivan@gmail.com>
2021-11-02 23:07:29 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6_2_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			const int n = 15;
+			int a[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+			t = Sum(a, n);
+			Assert::AreEqual(49, t);
+		}
+	};
+}
---
+++ lab_6_2_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+void Print(int* a, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        a[i] = i;
+        cout << setw(4) << a[i];
+    }
+    cout << endl;
+}
+int Sum(int* a, const int size)
+{
+    int S = 0;
+    for (int i = 0; i < size; i++)
+        if (a[i] % 2 == 1)
+            S += i;
+    return S;
+}
+
+int main()
+{
+    int n;
+    cout << "  " << "n = ";
+    cin >> n;
+    int* a = new int[n];
+    Print(a, n);
+    cout << "  " << "S = " << Sum(a, n) << endl;
+    delete[] a;
+    return 0;
+}
